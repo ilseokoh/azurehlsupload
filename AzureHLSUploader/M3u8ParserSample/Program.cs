6eb24fb0b9cc6bcb6a338d977c52f1e78784a9b2
오일석 <onestone@live.co.kr>
2017-06-01 19:02:16 +0900

    meu8 to file

--- AzureHLSUploader/M3u8ParserSample/Program.cs
+++ AzureHLSUploader/M3u8ParserSample/Program.cs
@@ -14,9 +14,7 @@ namespace M3u8ParserSample
 
             AzureHLSUploader.M3u8Parser parser;
 
-            parser = new AzureHLSUploader.M3u8Parser("https://s3-us-west-2.amazonaws.com/odk-hls-seg/v1/201705/religion/glory-church-of-jesus-christ/glory-church-of-jesus-christ-05312017-05312017/playlist_720.m3u8");
-
-            
+            parser = new AzureHLSUploader.M3u8Parser("https://s3-us-west-2.amazonaws.com/odk-hls-seg/drama/love-affair-agent/love-affair-agent-e01/playlist_1080.m3u8");
 
             Task.Run(async () =>
             {
@@ -24,6 +22,26 @@ namespace M3u8ParserSample
                 entry = await parser.ParseEntry();
                 Console.WriteLine(entry.Playlists.Count + entry.Playlists.Sum(x => x.TsFiles.Count) + 1);
                 Console.WriteLine(entry.Playlists.Count);
+
+                List<string> lines = new List<string>();
+                foreach(var playlist in entry.Playlists)
+                {
+                    lines.Add(playlist.Url);
+                    foreach(var tsfile in playlist.TsFiles)
+                    {
+                        lines.Add(tsfile);
+                    }
+                }
+                lines.Add(entry.Url);
+
+                using (System.IO.StreamWriter file =  new System.IO.StreamWriter(@"love affair.txt"))
+                {
+                    foreach(var line in lines)
+                    {
+                        file.WriteLine(line);
+                    }
+                }
+
             }).GetAwaiter().GetResult();
 
             
